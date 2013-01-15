e5165ceda9f0fdae65cc8054e495a82fbdb8b473
Jelte Fennema <jelte12345@gmail.com>
2013-01-15 14:22:23 +0100

    Use the new BaseEntity

--- models/Question.py
+++ models/Question.py
@@ -1,16 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, Boolean, DateTime
+from sqlalchemy import Column, String, Boolean
 from dbconnection import engine, session
-from datetime import datetime
+from Base import BaseEntity
 
 Base = declarative_base()
 
-class Question(Base):
+class Question(Base, BaseEntity):
     __tablename__ = 'questions'
 
-    id = Column(Integer, primary_key=True)
-    created = Column(DateTime, default=datetime.now)
-    modified = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     teacher_id = Column(String)
     question = Column(String)
     available = Column(Boolean)
@@ -25,10 +22,6 @@ class Question(Base):
         return "<Question ('%s','%s','%s')>" % (self.question,
                                                 self.teacher_id,
                                                 self.available)
-
-    @staticmethod
-    def by_id(id):
-        return session.query(Question).filter(Question.id == id).one()
 
 
 Base.metadata.create_all(engine)
