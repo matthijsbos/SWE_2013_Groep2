43ad3cb8d91c9c3ba53e99518156fd7a01bcf94d
Jelte Fennema <jelte12345@gmail.com>
2013-01-15 14:34:53 +0100

    Add get_all and by_ids to the base model

--- models/Base.py
+++ models/Base.py
@@ -9,6 +9,20 @@ class BaseEntity(object):
     created = Column(DateTime, default=datetime.now)
     modified = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
+
+    @classmethod
+    def get_all(self, session):
+        return session.query(self).filter().all()
+
+
     @classmethod
     def by_id(self, session, id):
         return session.query(self).filter(self.id == id).one()
+
+
+    @classmethod
+    def by_ids(self, sesion, ids):
+        if not ids:
+            return []
+
+        return session.query(self).filter(self.id.in_(ids)).all()
