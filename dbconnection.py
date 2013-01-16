f0fb49c9ff38bd6f30265cee60dd4fe8ae1fbffa
Laurens Verspeek <laurens_verspeek@hotmail.com>
2013-01-16 12:04:56 +0100

    deleted files

--- dbconnection.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import  *
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine('sqlite:///db.sqlite')
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
-
