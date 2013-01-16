3fc5b25619f4399a58db701611a285b9a52480ed
Juvawa <justinvwageningen@gmail.com>
2013-01-16 11:19:33 +0100

    edited add_question

--- controllers/handleQuestion.py
+++ controllers/handleQuestion.py
@@ -1,6 +1,7 @@
 # Author: Daniel Sepers
 # Description: This file handles inserting the received question into a database
 from flask import render_template
+from dbconnection import session
 
 class HandleQuestion():
     question = ""
@@ -10,7 +11,8 @@ class HandleQuestion():
 
     def add_question(self,question):
         self.question = question
-        # insert question into database here
+        session.add("teacher1", "", question, False)
+        session.commit()
     
     def render(self):
         return render_template('handleQuestion.html',question=self.question)
