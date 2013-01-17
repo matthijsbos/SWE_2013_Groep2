b2d96d3c9a41b8f5b1732df0d717b08f2a149f47
Koen Koning <koenkoning@gmail.com>
2013-01-17 20:39:32 +0100

    Begin of better index

--- controllers/index.py
+++ controllers/index.py
@@ -1,8 +1,28 @@
 from flask import render_template, g
 
+
 class Index():
-    def __init__(self, request):
-        self.request = request
+    def __init__(self, debug=False):
+        self.debug = debug
 
     def render(self):
-        return render_template('index.html', lti_dump=g.lti.dump_all())
+        if self.debug:
+            return render_template('index_debug.html',
+                                   lti_dump=g.lti.dump_all())
+        if g.lti.is_instructor():
+            return render_template('index_instructor.html')
+
+        return render_template('index_student.html')
+
+    def has_new_question(self):
+        if g.lti.is_instructor():
+            return json.dumps({'has_new': False})
+
+        # TODO: call function from question controller/model
+        ids = Question.get_unanswered()
+        question = Answer.get_id(ids[0])
+        # TODO: send first unanswered
+        return json.dumps({'has_new': True,
+                           'question_id': question.id,
+                           'question_text': question.question,
+                           'time_remaining': question.time})
