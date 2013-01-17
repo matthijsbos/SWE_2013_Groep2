4db45b137ac558dc128995658d799ff7f3fc4a58
Ruben Janssen <rubenljanssen@hotmail.com>
2013-01-17 13:40:45 +0100

    added unit test shit, not finished yet

--- unittests/unittest.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from os import name, path, rename, getcwd
-
-if __name__ == '__main__':
-    directory_separator = ('/' if name == "posix" else '\\')
-    src = path.abspath(path.join(getcwd(), path.pardir))+directory_separator+"db.sqlite"
-    dst = path.abspath(path.join(getcwd(), path.pardir))+directory_separator+"db_backup.sqlite"
-    rename(src, dst)
