e9c507957514ef761da8affbcc9f778bcec69c0d
v1kko <azizivictor@gmail.com>
2013-01-18 13:40:11 +0100

    Now working review thingy

---
+++ controllers/review.py
@@ -0,0 +1,50 @@
+# Authors : Victor Azizi & David Schoorisse & Mustafa Karaalioglu
+# Descrp : Controls the reviewing of answers
+# Changes:
+# Comment: call ReviewAnswer.review(x) to start reviewing a answer
+
+from flask import render_template, session as fsession
+from models.tag import Tag, AnswerTag
+from models.answer import AnswerModel
+from models.rating import AnswerRating, Rating
+from dbconnection import session
+
+class ReviewAnswer():
+    def __init__(self, request):
+        """
+        Handle the reviewed answer
+        """
+        try:
+            fsession['reviewanswer']
+        except:
+            return "YOU HAVE NO PERMISSION YOU FOOL"
+
+        for tag_id in request.form.getlist('assign_tags'):
+            AnswerTag.add_answertag(fsession['reviewanswer'], tag_id)
+
+        for rating_id in request.form.getlist('rating'):
+            AnswerRating.add_answerrating(fsession['reviewanswer'], rating_id)
+
+        for tag_id in request.form.getlist('remove_tags'):
+            AnswerTag.remove(fsession['reviewanswer'], tag_id)
+
+        # revoke permission to review answer
+        del fsession['reviewanswer']
+    
+    @staticmethod
+    def review(answer_id):
+
+        try:
+            answer = AnswerModel.by_id(answer_id)
+        except:
+            return "Error answer not found"
+        if answer == None:
+            return "Error answer not found"
+
+        fsession['reviewanswer'] = answer_id
+
+        enabledtags = AnswerTag.get_tag_ids(answer_id)
+
+        return render_template('reviewanswer.html', answer=answer,
+                               tags=Tag.get_all(), enabledtags=enabledtags)
+        
