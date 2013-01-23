5a651643e5628b638316885cc4de79e24f2f19a9
Tim <Bloeme>
2013-01-23 15:57:59 +0100

    data class geimplementeerd average werkt niet moet nog herschreven worden

--- clusterer.py
+++ clusterer.py
@@ -30,9 +30,20 @@ def test():
   print best_error
   print best_clustering
   print clustering_list[best_clustering].clusters
-    
+
 
 class Data():
+  name=""
+  text=""
+  vector=[]
+  
+  def __init__(self):
+    self.name=""
+    self.text=""
+    self.vector=[]
+
+
+class Vector_creation():
   answers = []
   tokens = []
   term_frequency = []
@@ -44,8 +55,7 @@ class Data():
   
   # add answer to list
   def add_answer(self,answer):
-    # lemmatization goes here
-    self.answers.append(answer.lower())
+    self.answers.append(answer)
   
   # tokenizer placeholder
   def tokenize_string(self,str):
@@ -53,10 +63,9 @@ class Data():
   
   # tokenize all answers, generate list of tokens and create term frequency matrix
   def tokenize_all(self):
-    for str in self.answers:
-      toks = self.tokenize_string(str)
-      print toks
-      for token in toks:
+    for n in range(len(answers)):
+      tokens = self.tokenize_string(answers[n].text)
+      for token in tokens:
         if token not in self.tokens:
           self.tokens.append(token)
     # initialize term frequency matrix
@@ -65,8 +74,8 @@ class Data():
   # counts the frequency of each term in each answer and adds them to the term frequency matrix
   def count_frequency(self):
     for i in range(len(self.answers)):
-      toks = self.tokenize_string(self.answers[i])
-      for token in toks:
+      tokens = self.tokenize_string(self.answers[i])
+      for token in tokens:
         for j in range(len(self.tokens)):
           if token == self.tokens[j]:
             self.term_frequency[i][j] = 1
@@ -75,7 +84,7 @@ class Data():
     
 class N_random():
   n = 0
-  vectors = []
+  answers = []
   set_n = 0
   selected = []
   clusters = []
@@ -96,14 +105,14 @@ class N_random():
     print self.clusters
     
   def distance(self,q1,q2):
-    vector1 = self.vectors[q1]
-    vector2 = self.vectors[q2]
+    vector1 = self.answers[q1].vector
+    vector2 = self.answers[q2].vector
     result = vector1 * vector2
     length = np.sum(result)
     return length
   
-  def add_vector(self,vector):
-    self.vectors.append(vector)
+  def add_vector(self,answer):
+    self.answers.append(answer)
   
   def calc_n(self):
     self.n = int(log(len(self.vectors)))
@@ -114,27 +123,30 @@ class N_random():
       if self.n < 1:
         self.n = 1
     for i in range(self.n):
-      a = random.randint(0,len(self.vectors)-1)
-      while self.vectors[a] in self.selected:
-        a = random.randint(0,len(self.vectors)-1)
-      self.selected.append(self.vectors[a])
+      a = random.randint(0,len(self.answers)-1)
+      while self.answers[a].vector in self.selected:
+        a = random.randint(0,len(self.answers)-1)
+      self.selected.append(self.answers[a].vector)
       self.clusters.append([])
   
   def assign_cluster(self):
-    for v in self.vectors:
+    for n in range(len(self.answers)):
+    vector=self.answers[n].vector
       best_dist = 0
       best_selected = 0
       for s in range(len(self.selected)):
-        dist = self.distance(v,self.selected[s])
+        dist = self.distance(vector,self.selected[s])
         if dist > best_dist:
           best_dist = dist
           best_selected = s
-      self.clusters[best_selected].append(v)
+      self.clusters[best_selected].append(self.answers[n])
       
   def calc_average_error(self,cluster):
+    #average moet opnieuw gaat compiler fout maken
     average = np.average(np.array(cluster),0)
     error = 0
-    for c in cluster:
+    for n in range(len(cluster)):
+      vector=cluster[n].vector
       diff = np.abs(c - average)
       error += np.sum(diff)
     self.error=error
