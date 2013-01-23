3d11fe426ec527132a5f3dfea2f924146bf660ab
Daniel Sepers <kidssepers@hotmail.com>
2013-01-23 14:12:35 +0100

    fixed testing function in clusterer.py

--- clusterer.py
+++ clusterer.py
@@ -1,7 +1,6 @@
 import numpy as np
-from Math import *
+from math import *
 import random
-
 
 def test():
   clustering_list = []
@@ -9,21 +8,26 @@ def test():
   best_error=99999999999
   best_clustering = nr_tries+1
   
+  lemma_answers = ["ja","nee"]
+  
   data=Data()
   
-  for answer in lemma_answers
+  for answer in lemma_answers:
     data.add_answer(answer)
   data.tokenize_all()
   data.count_frequency()
   
   for n in range(nr_tries):
-    clustering_list[n]=N_random()
-    for vector in range(len(data.term_frequency))
+    clustering_list.append(N_random())
+    for vector in range(len(data.term_frequency)):
       clustering_list[n].add_vector(vector)
     clustering_list[n].execute()
     if clustering_list[n].error < best_error:
       best_clustering = n
       best_error=clustering_list[n].error
+      
+  print best_error
+  print best_clustering
     
 
 class Data():
@@ -47,20 +51,20 @@ class Data():
   
   # tokenize all answers, generate list of tokens and create term frequency matrix
   def tokenize_all(self):
-    for str in answers:
-      toks = tokenize_string(str)
+    for str in self.answers:
+      toks = self.tokenize_string(str)
       for token in toks:
-        if token not in tokens:
-          self.tokens.append(t)
+        if token not in self.tokens:
+          self.tokens.append(token)
     # initialize term frequency matrix
-    self.term_frequency = np.zeros((len(answers),len(tokens)))
+    self.term_frequency = np.zeros((len(self.answers),len(self.tokens)))
   
   # counts the frequency of each term in each answer and adds them to the term frequency matrix
   def count_frequency(self):
-    for i in range(len(answers)):
-      toks = tokenize_string(answers[i])
+    for i in range(len(self.answers)):
+      toks = self.tokenize_string(self.answers[i])
       for token in toks:
-        for k in range(len(tokens)):
+        for k in range(len(self.tokens)):
           if token == k:
             self.term_frequency[i][j] = 1
     
@@ -78,15 +82,16 @@ class N_random():
     self.selected = []
     self.clusters = []
     
-  def execute():
-    calc_n()
-    select_n()
-    assign_cluster()
-    calc_average_error()
+  def execute(self):
+    self.calc_n()
+    self.select_n()
+    self.assign_cluster()
+    for c in self.clusters:
+      self.calc_average_error(c)
     
-  def distance(q1,q2):
-    vector1 = term_frequency[q1]
-    vector2 = term_frequency[q2]
+  def distance(self,q1,q2):
+    vector1 = self.vectors[q1]
+    vector2 = self.vectors[q2]
     result = vector1 * vector2
     length = np.sum(result)
     return length
@@ -95,32 +100,32 @@ class N_random():
     self.vectors.append(vector)
   
   def calc_n(self):
-    self.n = int(log(len(vectors)))
+    self.n = int(log(len(self.vectors)))
     
   def select_n(self):
-    if n < 1:
-      calc_n()
-      if n < 1:
+    if self.n < 1:
+      self.calc_n()
+      if self.n < 1:
         self.n = 1
-    for i in range(n):
-      a = random.randint(0,len(vectors)-1)
-      while vectors[a] in selected:
-        a = random.randint(0,len(vectors)-1)
-      self.selected.append(vectors[a])
+    for i in range(self.n):
+      a = random.randint(0,len(self.vectors)-1)
+      while self.vectors[a] in self.selected:
+        a = random.randint(0,len(self.vectors)-1)
+      self.selected.append(self.vectors[a])
       self.clusters.append([])
   
   def assign_cluster(self):
-    for v in vectors:
+    for v in self.vectors:
       best_dist = 0
       best_selected = 0
-      for s in selected:
-        dist = distance(v,s)
+      for s in range(len(self.selected)):
+        dist = self.distance(v,self.selected[s])
         if dist > best_dist:
           best_dist = dist
           best_selected = s
       self.clusters[best_selected].append(v)
       
-  def calc_average_error(cluster):
+  def calc_average_error(self,cluster):
     average = np.average(np.array(cluster),0)
     error = 0
     for c in cluster:
